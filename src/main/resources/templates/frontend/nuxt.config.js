cec1a2b48e9ec8053ca173225408de8ebcb349db
Wouter Nederhof <wouter.nederhof@gmail.com>
2020-10-06 00:06:15 +0200

    Fix unit tests.

--- src/main/resources/templates/frontend/nuxt.config.js
+++ src/main/resources/templates/frontend/nuxt.config.js
@@ -34,6 +34,7 @@ export default {
   ** https://nuxtjs.org/guide/plugins
   */
   plugins: [
+    '@/plugins/vue-uid.ts'
   ],
   /*
   ** Auto import components
---
+++ src/main/resources/templates/frontend/src/main/webapp/plugins/vue-uid.ts
@@ -0,0 +1,4 @@
+import Vue from 'vue'
+import UniqueId from 'vue-unique-id'
+
+Vue.use(UniqueId)
