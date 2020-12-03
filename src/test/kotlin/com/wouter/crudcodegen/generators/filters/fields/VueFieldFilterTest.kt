a8113d85f47431a3bd3552b2be71de26231f02d8
Wouter Nederhof <wouter.nederhof@gmail.com>
2020-12-03 23:14:30 +0100

    Add missing test cases for different field filters.

---
+++ src/test/kotlin/com/wouter/crudcodegen/generators/filters/fields/VueFieldFilterTest.kt
@@ -0,0 +1,55 @@
+package com.wouter.crudcodegen.generators.filters.fields
+
+import com.wouter.crudcodegen.generators.EntityType
+import com.wouter.crudcodegen.generators.filters.EntityField
+import com.wouter.crudcodegen.generators.filters.FieldTemplateFilter
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.EnumSource
+import org.mockito.InjectMocks
+import org.mockito.junit.jupiter.MockitoExtension
+
+@ExtendWith(MockitoExtension::class)
+class VueFieldFilterTest {
+    @InjectMocks
+    private lateinit var databaseFieldFilter: DatabaseFieldFilter
+
+    @Test
+    fun `enrichProperties yields the fieldHtmlInputType number for any relation`() {
+        val fields = listOf(EntityField.RelationalEntityField("userId", "User"))
+        val settings = FieldTemplateFilter.FieldTemplateSettings(SOME_STRING, SOME_STRING, SOME_STRING, fields)
+
+        val singleActualVariable = databaseFieldFilter.enrichProperties(0, settings).single()
+
+        assertThat(singleActualVariable.name).isEqualTo("fieldHtmlInputType")
+        assertThat(singleActualVariable.value).isEqualTo("number")
+    }
+
+    @Test
+    fun `enrichProperties may yield a different fieldHtmlInputType for a primitive than for a relation`() {
+        val fields = listOf(EntityField.PrimitiveEntityField("name", EntityType.STRING))
+        val settings = FieldTemplateFilter.FieldTemplateSettings(SOME_STRING, SOME_STRING, SOME_STRING, fields)
+
+        val singleActualVariable = databaseFieldFilter.enrichProperties(0, settings).single()
+
+        assertThat(singleActualVariable.name).isEqualTo("fieldHtmlInputType")
+        assertThat(singleActualVariable.value).isEqualTo("VARCHAR(255) NOT NULL")
+    }
+
+    @ParameterizedTest
+    @EnumSource(EntityType::class)
+    fun `enrichProperties yields a fieldHtmlInputType for every primitive`(entityType: EntityType) {
+        val fields = listOf(EntityField.PrimitiveEntityField("userId", entityType))
+        val settings = FieldTemplateFilter.FieldTemplateSettings(SOME_STRING, SOME_STRING, SOME_STRING, fields)
+
+        val singleActualVariable = databaseFieldFilter.enrichProperties(0, settings).single()
+
+        assertThat(singleActualVariable.name).isEqualTo("fieldHtmlInputType")
+    }
+
+    private companion object {
+        const val SOME_STRING = "some-irrelevant-string"
+    }
+}
